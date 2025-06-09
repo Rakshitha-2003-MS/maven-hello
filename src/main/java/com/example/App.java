4302dae4950c02b6ad0d450c255f6efb62a80623
Rakshitha-2003 <rakshithams106@gmail.com>
2025-06-09 15:37:00 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Maven" );
+        System.out.println( "I'm Here" );
+    }
+}
